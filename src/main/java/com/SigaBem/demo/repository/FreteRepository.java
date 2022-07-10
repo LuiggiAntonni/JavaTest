24dcfb7480b1f4a2f8a8add9d29ed5d42d5d07d3
Luiggi Antonni <luiggiantonni0129@gmail.com>
2022-07-10 17:55:41 -0300

    Mudança de Crud para JpaRepository

--- src/main/java/com/SigaBem/demo/repository/FreteRepository.java
+++ src/main/java/com/SigaBem/demo/repository/FreteRepository.java
@@ -1,10 +1,14 @@
 package com.SigaBem.demo.repository;
 
+import com.SigaBem.demo.model.dto.FreteResponse;
 import com.SigaBem.demo.model.entity.FreteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface FreteRepository extends CrudRepository<FreteEntity, Long> {
+import java.util.List;
 
+@Repository
+public interface FreteRepository extends JpaRepository<FreteEntity, Long> {
+    
 }
