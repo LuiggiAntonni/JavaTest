d25186e895b4b3afc9730b3ed3a3d232da55567e
Luiggi Antonni <luiggiantonni0129@gmail.com>
2022-07-10 10:05:21 -0300

    Calculo do frete

--- src/main/java/com/SigaBem/demo/service/FreteService.java
+++ src/main/java/com/SigaBem/demo/service/FreteService.java
@@ -4,6 +4,15 @@ import com.SigaBem.demo.model.dto.FreteRequest;
 import com.SigaBem.demo.model.dto.FreteResponse;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 public interface FreteService {
     FreteResponse calcFrete(FreteRequest req);
+
+    default BigDecimal calculoDeDesconto(Double desconto, Double peso, BigDecimal valorPorKg) {
+        var desc = new BigDecimal(desconto).multiply(new BigDecimal(peso));
+        var totalFreteSemDesc= new BigDecimal(peso).multiply(valorPorKg);
+        return totalFreteSemDesc.subtract(desc);
+
+    }
 }
--- src/main/java/com/SigaBem/demo/service/impl/FreteServiceImpl.java
+++ src/main/java/com/SigaBem/demo/service/impl/FreteServiceImpl.java
@@ -1,12 +1,15 @@
 package com.SigaBem.demo.service.impl;
 
 import com.SigaBem.demo.client.ApiCepClient;
+import com.SigaBem.demo.client.model.ApiCepResponse;
 import com.SigaBem.demo.model.dto.FreteRequest;
 import com.SigaBem.demo.model.dto.FreteResponse;
 import com.SigaBem.demo.service.FreteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Slf4j
 @Service
@@ -23,7 +26,26 @@ public class FreteServiceImpl implements FreteService {
                 .cepOrigem(req.getCepOrigem())
                 .cepDestino(req.getCepDestino())
                 .build();
-        log.info(String.valueOf(ApiReqCepDestino.getDdd()));
+
+        descontoDoFrete(freteResponse, req.getPeso(), ApiReqCepOrigem, ApiReqCepDestino);
+
         return freteResponse;
+    }
+
+    private void descontoDoFrete(FreteResponse freteResponse, Double peso, ApiCepResponse apiReqCepOrigem, ApiCepResponse apiReqCepDestino) {
+        var valorPorKg = new BigDecimal(1);
+        var desconto = 0.0;
+        var prevDiasEntrega = 10;
+
+        if (apiReqCepOrigem.getDdd().equals(apiReqCepDestino.getDdd())){
+            desconto = 0.5;
+            prevDiasEntrega = 1;
+        } else if (apiReqCepOrigem.getUf().equals(apiReqCepDestino.getUf())) {
+            desconto = 0.75;
+            prevDiasEntrega = 3;
+        }
+
+        freteResponse.setVlTotalFrete(calculoDeDesconto(desconto, peso, valorPorKg));
+        freteResponse.setDataPrevistaEntrega(prevDiasEntrega);
     }
 }
